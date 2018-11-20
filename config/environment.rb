e096a92ea31affd6cf4a3eab4a670f996fa376d6
Jonathan Tuzman <tuzmusic@gmail.com>
2018-11-20 14:35:23 +0000

    execute first migration

--- config/environment.rb
+++ config/environment.rb
@@ -3,6 +3,8 @@ Bundler.require
 
 ENV['SINATRA_ENV'] ||= "development"
 
+ActiveRecord::Base.logger = nil
+
 ActiveRecord::Base.establish_connection(
   :adapter => "sqlite3",
   :database => "db/user_auth#{ENV['SINATRA_ENV']}.sqlite"
