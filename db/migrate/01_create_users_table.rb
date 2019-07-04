30d4f6afea0dcf5abf95c7ad77f1be57bd3cfe7b
Yingqi Chen <chanwk18@hotmail.com>
2019-07-04 00:16:25 +0000

    Done.

---
+++ db/migrate/01_create_users_table.rb
@@ -0,0 +1,14 @@
+class CreateUsersTable < ActiveRecord::Migration
+
+
+  def change
+    create_table :users do |t|
+     t.string :name
+     t.string :email
+     t.string :password
+   end
+ end
+
+
+
+end
