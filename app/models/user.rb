b83769fe9218d101a606a004e2a18f874fc3854f
Malcolm Leake <echelon1089@gmail.com>
2019-07-18 20:17:16 +0000

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,13 @@
 class User < ActiveRecord::Base
   validates_presence_of :name, :email, :password
+
+
+  def change
+    create_table :Users do |t|
+      t.string :name
+      t.string :email 
+      t.string :password
+    end
+  end
+ 
 end
