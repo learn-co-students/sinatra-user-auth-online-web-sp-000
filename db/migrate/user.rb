45da159697b3c733f1416d414b11b0bd990e735d
Malcolm Leake <echelon1089@gmail.com>
2019-07-18 20:27:48 +0000

    Done.

---
+++ db/migrate/user.rb
@@ -0,0 +1,10 @@
+class User < ActiveRecord::Migration[5.2]
+  def change
+    create_table :user do |t|
+      t.string :name
+      t.string :email 
+      t.string :password 
+    end
+  end
+ 
+end
