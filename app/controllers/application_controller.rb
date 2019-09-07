65fb89a2ec6650934276381073001ed9030e45a8
rohanphillips <rohan@solutions-insurance.com>
2019-09-07 08:48:53 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,7 +17,6 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/registrations' do
-    puts params
     @user = User.new(name: params["name"], email: params["email"], password: params["password"])
     @user.save
     session[:user_id] = @user.id
@@ -46,7 +45,11 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/users/home' do
-    @user = User.find(session[:user_id])
-    erb :'/users/home'
+    if session[:user_id] != nil
+      @user = User.find(session[:user_id])
+      erb :'/users/home'
+    else
+      redirect '/registrations/signup'
+    end
   end
 end
