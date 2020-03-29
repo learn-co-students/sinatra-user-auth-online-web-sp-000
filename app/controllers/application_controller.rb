7008cecf8aee8e333302dd3dd28e67608f83ab6a
Kirsten Keels <keelsk@gmail.com>
2020-03-29 23:49:36 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -27,6 +27,7 @@ class ApplicationController < Sinatra::Base
   get '/sessions/login' do
 
     # the line of code below render the view page in app/views/sessions/login.erb
+    
     erb :'sessions/login'
   end
 
