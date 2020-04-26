706699f15699b36eddf59fb003c6f7c81baf8a20
dkw5011 <dkw5011@gmail.com>
2020-04-25 21:45:00 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,7 +20,7 @@ class ApplicationController < Sinatra::Base
     @user = User.new(name: params["name"], email: params["email"], password: params["password"])
     @user.save
     session[:user_id] = @user.id
-
+    puts params
     redirect '/users/home'
   end
 
@@ -35,6 +35,7 @@ class ApplicationController < Sinatra::Base
     if @user
       session[:user_id] = @user.id
       redirect '/users/home'
+      puts params
     end
     redirect '/sessions/login'
   end
