f3e5712b652665bf364e7f15d43f1db199292647
Deborah Barcenas <deborah.barcenas@gmail.com>
2020-06-15 19:20:26 +0000

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,32 @@
+
+class SessionsController < ApplicationController
+
+
+  get '/sessions/login' do
+
+    # the line of code below render the view page in app/views/sessions/login.erb
+    erb :'sessions/login'
+  end
+
+  post '/sessions' do
+    # find the user who submitted the log in forms by looking in your database
+    #   for the user with the email and password from the params
+    # sign them in by setting the session[:id] equal to the user's id
+
+    # redirect the user to this route: get '/users/home'
+    #  that route is in the Users Controller. Go check out the code there.
+    @user = User.find_by(email: params["email"], password: params["password"])
+    if @user
+      session[:id] = @user.id
+      redirect '/users/home'
+    end
+    redirect '/sessions/login'
+  end
+
+  get '/sessions/logout' do
+    # log out the user by clearing the session hash here
+    session.clear
+    redirect '/'
+  end
+
+end
