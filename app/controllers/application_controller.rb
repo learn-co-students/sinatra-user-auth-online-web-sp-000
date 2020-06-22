d5f85036779b5cf950cf4230d8cb4ec606aabf72
murphcoder <murphcoder@gmail.com>
2020-06-22 17:56:04 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,7 +17,7 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/registrations' do
-    @user = User.new(name: params["name"], email: params["email"], password: params["password"])
+    @user = User.new(name: params[:name], email: params[:email], password: params[:password])
     @user.save
     session[:user_id] = @user.id
 
