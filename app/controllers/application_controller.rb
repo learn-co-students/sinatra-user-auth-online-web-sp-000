27ef850d37e7dd3168709493300515490df2bf9a
ting682 <tchung682@gmail.com>
2020-07-09 09:37:28 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,9 +19,10 @@ class ApplicationController < Sinatra::Base
   post '/registrations' do
     @user = User.new(name: params["name"], email: params["email"], password: params["password"])
     @user.save
+    binding.pry
     session[:user_id] = @user.id
 
-    redirect '/users/home'
+    redirect to '/users/home'
   end
 
   get '/sessions/login' do
