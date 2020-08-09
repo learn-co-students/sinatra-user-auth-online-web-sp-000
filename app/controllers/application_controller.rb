6a02f345135e118df6ec4e9d3c5c9ef48664c90d
github username <github email address>
2020-08-09 11:01:28 -0600

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,20 +12,16 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/registrations/signup' do
-
     erb :'/registrations/signup'
   end
 
   post '/registrations' do
-    @user = User.new(name: params["name"], email: params["email"], password: params["password"])
-    @user.save!
+    @user = User.create(name: params["name"], email: params["email"], password: params["password"])
     session[:user_id] = @user.id
     redirect '/users/home'
   end
 
   get '/sessions/login' do
-
-    # the line of code below render the view page in app/views/sessions/login.erb
     erb :'sessions/login'
   end
 
