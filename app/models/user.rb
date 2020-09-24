54e964ca7d9da077eaa15e8d448348f8a894bc40
reevesalic <reevesalic@gmail.com>
2020-09-24 12:48:32 -0400

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,12 @@
 class User < ActiveRecord::Base
-  validates_presence_of :name, :email, :password
+  validates_presence_of :name, :email, :password, :users
+
+  # attr_accessor :name, :email, :password
+
+  def initialize(name, email, password, users)
+    @name = name
+    @email = email
+    @password = password
+    @users = users
+  end
 end
