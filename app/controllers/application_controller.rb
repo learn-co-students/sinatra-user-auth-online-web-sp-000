37697c167fdcb4f37e3a542a406d218fdd8cba3e
mrandolph83 <mrandolph83@gmail.com>
2021-05-01 16:52:05 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class ApplicationController < Sinatra::Base
   register Sinatra::ActiveRecordExtension
   set :views, Proc.new { File.join(root, "../views/") }
@@ -20,6 +21,8 @@ class ApplicationController < Sinatra::Base
     @user = User.new(name: params["name"], email: params["email"], password: params["password"])
     @user.save
     session[:user_id] = @user.id
+
+    # puts params
 
     redirect '/users/home'
   end
--- db/migrate/20210501193206_create_users.rb
+++ db/migrate/20210501193206_create_users.rb
@@ -1,11 +1,9 @@
-class User < ActiveRecord::Base
-  validates_presence_of :name, :email, :password
-
+class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
       t.string :name
       t.string :email
       t.string :password
-    end
+    end 
   end
 end
